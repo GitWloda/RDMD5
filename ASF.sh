0da9f1f004ef2ef18cf9079dfdeca49c81f50b36
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-10-21 23:46:35 +0200

    Update ASF.sh

--- ASF.sh
+++ ASF.sh
@@ -39,7 +39,7 @@ case $1 in
 				esac
 			fi
 			mkdir ~/.ASFinstall
-			cd ~/.ASFinstallz
+			cd ~/.ASFinstall
 			git clone https://github.com/GitWloda/AutoSortFile.git
 			cd AutoSortFile
 			chmod 777 remASF.sh
