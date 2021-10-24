44bacedcd7e13b222f2d36e0571ed7f56fe64a88
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-10-24 22:30:30 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#0.3b
+#1.0a
 
 if [[ $(eval "whoami") == "root" ]]; then
 rm /usr/bin/ASF.sh
 chmod 777 ASF.sh
-mv ./ASF.sh /usr/bin/
+cp ./ASF.sh /usr/bin/
 rm -rf ~/.ASFinstall &
 else 
 	echo "please use sudo"
