8e77762d0238c637fa91d422b110b48dac7f0c7d
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-10-25 15:44:35 +0200

    Update ASF.sh

--- ASF.sh
+++ ASF.sh
@@ -23,14 +23,15 @@ prog(){
 	mysql --user=root -se "drop database if exists ASF;
 		create database ASF;
 		use ASF;
-		create table ASFtable (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name varchar(128), date DATE);"
+		create table ASFtable (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name varchar(128), date DATE, checksum varchar(128));"
 	while [[ $NRFile > 0 ]]; do
 		cd $(eval "echo $dirfrom")
 		echo -ne "\e[96m$NRFile\033[0K\r"
 		nomeFile=$(ls -1 $(eval "echo $dirfrom") | tail -$NRFile | head -n1)
 		data=$(stat $(eval "echo $dirfrom")/$nomeFile | tail -3 | head -n1 | cut -d ':' -f 2 | awk {'print $1'})
+		codMD5=$(md5sum $nomeFile | awk {'print $1'} )
 		mysql --user=root -se "use ASF;
-		insert into ASFtable (name, date) values ('$nomeFile','$data');"
+		insert into ASFtable (name, date, checksum) values ('$nomeFile','$data','$codMD5');"
 		let NRFile=NRFile-1
 	done
 		mysql --user=root -se "use ASF; SELECT * from ASFtable"
